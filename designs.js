0e467f402b9bbbe1bfa6b9841234f5527d36fa4a
karabatovatanya <62766210+karabatovatanya@users.noreply.github.com>
2022-05-13 13:11:18 +0200

    Update designs.js

--- designs.js
+++ designs.js
@@ -5,18 +5,18 @@ var height = document.getElementById('#inputHeight');
 var width = document.getElementById('#inputWidth');
 // When size is submitted by the user, call makeGrid()
 var tableSize = document.getElementById('#sizePicker');
-tableSize.addEventListener(function (event) {
+tableSize.addEventListener('submit', function (event) {
   event.preventDefault();
+  var height = document.getElementById('inputHeight').value;
+  var width = document.getElementById('inputWidth').value;
   makeGrid(height, width);
-})
+});
 
 function makeGrid(height, width) {
   var table =  document.getElementById('#pixelCanvas');
-  var rows = height.value;
-  var columns = width.value;
-  for (var i = 1; i <= rows; i++) {
+  for (var i = 1; i <= height; i++) {
     var row = document.createElement('tr');
-    for (var j = 1; j <= columns; j++) {
+    for (var j = 1; j <= width; j++) {
       var column = document.createElement('td');
       row.appendChild(column);
     }
