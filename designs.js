fbda3902250c268c7986eacd7e83f591c631df31
karabatovatanya <62766210+karabatovatanya@users.noreply.github.com>
2022-05-13 12:46:14 +0200

    Update designs.js

--- designs.js
+++ designs.js
@@ -3,13 +3,13 @@ var color = document.getElementById('#colorPicker');
 // Select size input
 var height = document.getElementById('#inputHeight');
 var width = document.getElementById('#inputWidth');
-var table =  document.getElementById('#pixelCanvas')
+var table =  document.getElementById('#pixelCanvas');
 // When size is submitted by the user, call makeGrid()
 var tableSize = document.getElementById("#sizePicker");
 tableSize.addEventListener('submit', function (event) {
   event.preventDefault();
   makeGrid(height.value, width.value)
-  }
+})
 
 function makeGrid(height.value, width.value) {
   for (var i = 1; i <= height; i++) {
